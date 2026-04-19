4d43a7b1daec11797228a7b63905726378f86202
isha190504 <ishamte1905@gmail.com>
2026-04-19 19:59:57 +0530

    Update UserRecord.java

--- src/main/java/com/jpmc/midascore/entity/UserRecord.java
+++ src/main/java/com/jpmc/midascore/entity/UserRecord.java
@@ -1,5 +1,11 @@
 package com.jpmc.midascore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
 import jakarta.persistence.*;
 
 @Entity
