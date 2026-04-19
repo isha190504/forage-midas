f8dff618b3c9bf8bb14f64acf74711827235c086
isha190504 <ishamte1905@gmail.com>
2026-04-19 21:27:27 +0530

    Update DatabaseConduit.java

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,29 @@
 package com.jpmc.midascore.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.UserRepository;
-import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseConduit {
-    private final UserRepository userRepository;
 
-    public DatabaseConduit(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @KafkaListener(topics = "transactions", groupId = "midas-group")
+    public void listen(String message) {
+
+        // Example message format: "Alice,100.0"
+        String[] parts = message.split(",");
+
+        String name = parts[0];
+        Float amount = Float.parseFloat(parts[1]);
+
+        UserRecord user = new UserRecord(name, amount);
+
+        userRepository.save(user);
     }
-
-    public void save(UserRecord userRecord) {
-        userRepository.save(userRecord);
-    }
-
 }
