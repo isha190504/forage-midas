de873b0e7091f930e88515b602369c6e5590d6e1
isha190504 <ishamte1905@gmail.com>
2026-04-19 21:29:03 +0530

    Update DatabaseConduit.java

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,10 +1,10 @@
 package com.jpmc.midascore.component;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.UserRepository;
 
 @Component
@@ -13,17 +13,26 @@ public class DatabaseConduit {
     @Autowired
     private UserRepository userRepository;
 
-    @KafkaListener(topics = "transactions", groupId = "midas-group")
-    public void listen(String message) {
+    public void process(Transaction transaction) {
 
-        // Example message format: "Alice,100.0"
-        String[] parts = message.split(",");
+        // Find sender
+        UserRecord sender = userRepository.findByName(transaction.getSender());
 
-        String name = parts[0];
-        Float amount = Float.parseFloat(parts[1]);
+        // Find receiver
+        UserRecord receiver = userRepository.findByName(transaction.getRecipient());
 
-        UserRecord user = new UserRecord(name, amount);
+        if (sender != null && receiver != null) {
 
-        userRepository.save(user);
+            Float amount = transaction.getAmount();
+
+            // Deduct from sender
+            sender.setBalance(sender.getBalance() - amount);
+
+            // Add to receiver
+            receiver.setBalance(receiver.getBalance() + amount);
+
+            userRepository.save(sender);
+            userRepository.save(receiver);
+        }
     }
 }
