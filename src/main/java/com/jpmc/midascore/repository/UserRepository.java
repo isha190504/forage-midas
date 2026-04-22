2682247e79dae785c8fe6f242f2f9fda5acaad6e
isha190504 <ishamte1905@gmail.com>
2026-04-22 11:27:55 +0530

    Update UserRepository.java

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.jpmc.midascore.repository;
 
-import com.jpmc.midascore.entity.UserRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.jpmc.midascore.entity.UserRecord;
 
 public interface UserRepository extends JpaRepository<UserRecord, Long> {
+
+    UserRecord findByName(String name);
 }
